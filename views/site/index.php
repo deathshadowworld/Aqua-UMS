cc9e2306ea97fc6c867184faa9cb070050f5cbaf
Mohd Ridwan Roslan <mohammadridwan911@gmail.com>
2024-07-30 13:29:54 +0800

    hid forgot password

--- views/site/index.php
+++ views/site/index.php
@@ -51,8 +51,9 @@ $this->title = 'Aqua UMS Project';
 
             <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
-            <?= $form->field($model, 'password')->passwordInput() ?><a href="https://www.youtube.com">Forgot
-                password?</a></br></br>
+            <?= $form->field($model, 'password')->passwordInput() ?>
+            <!--<a href="https://www.youtube.com">Forgot
+                password?</a></br></br>-->
 
             <?= $form->field($model, 'rememberMe')->checkbox([
                 'template' => "<div class=\"offset-lg-1 col-lg-3 custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
